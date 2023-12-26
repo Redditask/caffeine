ef5590593ae64e9ce1548aa17f09e102ba6d553a
Redditask <artyomageyko@gmail.com>
2023-12-26 16:52:56 +0300

    used template

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(
+    cors({
+      origin: `*`,
+      methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'],
+      credentials: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('CAFFEINE')
+    .setDescription('CAFFEINE description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
